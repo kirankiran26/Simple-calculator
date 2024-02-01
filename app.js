916b7832da9dd180db9321ee2972eb0ffa26f899
Kiran P S <kiranps2607@gmail.com>
2024-02-01 13:20:40 +0530

    added the result div and functional opperators

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 let numbut = document.querySelectorAll(".num ,.result");
 let eq = "";
-
+let resultdivbt=document.querySelector(".resultdiv");
+console.log(resultdivbt.innerText);
 numbut.forEach(btt => {
     btt.addEventListener("click", () => {
         if (btt.innerText==='=') {
-            console.log("lool");
+            resultdivbt.append(btt.innerText)
+            let result=tocalculatetheresult(eq);
+            resultdivbt.append(result)
+            console.log(result);
+        }
+        else if (btt.innerText==='AC') {
+            resultdivbt.innerText="";
         }
         else {
+            resultdivbt.append(btt.innerText)
             eq = eq + btt.innerText;
             console.log(eq);
+            
         } 
     });
 });
+const tocalculatetheresult = (eq) => {
+    let result;
+    result=eval(eq);
+    return result;
+};
